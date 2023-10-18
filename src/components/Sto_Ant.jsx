327cb698c7b4810e58d91b4065cb3592b868c825
glayberson <glaybersonsilva@outlook.com>
2023-10-18 20:50:02 -0300

    alteração para svg em Sto_Ant

--- src/components/Sto_Ant.jsx
+++ src/components/Sto_Ant.jsx
@@ -8,7 +8,8 @@ function Sto_Ant() {
   <div className="bg-[url(bg/bgMission.jpg)] flex-col bg-cover bg-center h-max text-gray-50 font-michroma ">
    
     <div className="  flex justify-center "> 
-        <h1 className=" text-9xl lg:text-sto ">Sto Ant</h1>
+    <img src="Sto.svg" alt="" className="w-screen"/>
+        
     </div>
     <div className="lg:px-28 lg:text-8xl text-6xl flex-col py-60">
         <div className="lg:pb-72"> 
