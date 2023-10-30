bdda0b3baf6fb26e5a774e976c5cf2af5829a83e
glayberson <glaybersonsilva@outlook.com>
2023-10-30 20:41:57 -0300

    Alteração nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Navbar({name, go}) {
   return (
-    <nav className=' flex flex-row py-5 sm:px-2 lg:px-5 xl:px-14 leading-6'>
+    <nav className=' flex flex-row py-5 sm:px-2 lg:px-5 xl:px-14  bg-[#6E45F4]'>
         
         <div className='text-gray-50 bg-[#6E45F4] text-[0.4rem] lg:text-base w-12 h-6 sm:text-xs flex flex-row xl:w-40  lg:w-32 sm:w-24 sm:h-16 rounded-full items-center justify-center text-center'>
-            <a href={go}>{name}</a>
+            <a  className='leading-6' href={go}>{name}</a>
         </div>
     </nav>
   )
