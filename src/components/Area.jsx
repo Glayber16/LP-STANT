c9cce2a2e686b12fa04901139b6cfda4cf4808f5
Glayberson <glabersonsilva@outlook.com>
2023-10-30 15:00:48 -0300

    responsividade ajustada

--- src/components/Area.jsx
+++ src/components/Area.jsx
@@ -5,7 +5,7 @@ import Cards from './Cards';
 function Area() {
   return (
     <div id='area' className='bg-gradient-to-b from-area to-[#3728b4] font-michroma text-gray-50 flex flex-col py-6 '>
-        <div className=' xl:text-8xl 3xl:text-[3] md:text-4xl text-xl flex justify-center text-center  md:justify-end py-6 md:pr-11 2xl:pr-24 tracking-widest'> 
+        <div className=' xl:text-8xl 3xl:text-[3] md:text-4xl text-xl flex justify-center text-center  md:justify-end py-6 md:pr-11 2xl:pr-24 lg:[word-spacing:0.9rem]	'> 
             <h1>Áreas de atuação</h1>
         </div>
         <div className='flex flex-col md:flex-row py-5 justify-center items-center md:justify-around 2xk:px-10 3xl:px-36'> 
