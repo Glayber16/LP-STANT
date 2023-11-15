06483474d439bf920dc918a69d0de111c2a372ad
glayberson <glaybersonsilva@outlook.com>
2023-11-14 21:25:39 -0300

    retoques na responsividade

--- src/components/Pop_up2.jsx
+++ src/components/Pop_up2.jsx
@@ -9,12 +9,12 @@ function Pop_up2({name1, name2, name3, text}) {
 
       <div className=" 2xl:-mt-4  flex flex-col ml-8 lg:ml-0 xl:ml-20 lg:mr-20 3xl:mr-40 3xl:mt-2 ">
         
-        <button onClick={() => setPop_up (true)} className="cursor-pointer  med:max-sm:mt-5 sm:mb-0  mr-8 mt-2    lg:mr-24 xl:mr-20 2xl:mr-16 2xl:mt-16 ">{name1}</button>
+        <button onClick={() => setPop_up (true)} className="cursor-pointer  med:max-sm:mt-5 sm:mb-0  mr-8 mt-5    lg:mr-24 xl:mr-20 2xl:mr-16 2xl:mt-16 ">{name1}</button>
           
-          <button onClick={() => setPop_up (true)} className="cursor-pointer ml-32 med:max-sm:ml-[9.5rem]  sm:ml-48 mt-2 lg:mr-5 2xl:mr-0 lg:mt-10 xl:mt-12">{name2}</button>
+          <button onClick={() => setPop_up (true)} className="cursor-pointer ml-32 med:max-sm:ml-[9.5rem]  sm:ml-42 mt-2 lg:mr-5 2xl:mr-0 lg:mt-10 xl:mt-12">{name2}</button>
            
             
-          <button onClick={() => setPop_up (true)} className="cursor-pointer  xl:-mt-2 sm:mr-36 mr-28  lg:mt-0  xl:mb-0 lg:mr-56 xl:mr-52 2xl:mr-48">{name3}</button>
+          <button onClick={() => setPop_up (true)} className="cursor-pointer mt-4 xl:-mt-2 sm:mr-36 mr-28  lg:mt-0  xl:mb-0 lg:mr-56 xl:mr-52 2xl:mr-48">{name3}</button>
           
           {Pop_up && ( <div className='lg:w-screen h-screen fixed top-0 left-0 bottom-0 right-0 flex justify-center items-center '>
               <div onClick={() => setPop_up (!Pop_up)} className='w-screen h-screen fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-75 '> </div>
