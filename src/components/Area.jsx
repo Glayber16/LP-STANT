31b8319251db652b4f2b62dc96690e3975287493
Glayberson <glabersonsilva@outlook.com>
2023-11-14 21:38:26 -0300

    responsividade especifica para 1366px feita

--- src/components/Area.jsx
+++ src/components/Area.jsx
@@ -5,7 +5,7 @@ import Cards from './Cards';
 function Area() {
   return (
     <div id='area' className='bg-gradient-to-b from-area to-[#3728b4] font-michroma text-gray-50 flex flex-col py-24 '>
-        <div className=' lg:text-[82px] 3xl:text-[105px] md:text-4xl text-xl flex justify-center text-center  md:justify-end lg:pb-16 md:pr-11 xl:pr-28 3xl:pr-40 '> 
+        <div className=' lg:text-[82px] 3xl:text-[105px] md:text-4xl text-xl flex justify-center text-center  md:justify-end lg:pb-16 md:pr-11 xl:pr-28 note:pr-20 3xl:pr-40 '> 
             <h1>Áreas de atuação</h1>
         </div>
         <div className='flex flex-col md:flex-row py-5 justify-center items-center md:justify-around 2xl:px-20 3xl:px-36'> 
