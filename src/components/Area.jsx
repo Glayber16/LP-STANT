fdf638c95da88bc55598cc1a2078920a0e5f1919
Glayberson <glabersonsilva@outlook.com>
2023-11-14 21:29:17 -0300

    responsividade 1366 diferente

--- src/components/Area.jsx
+++ src/components/Area.jsx
@@ -8,7 +8,7 @@ function Area() {
         <div className=' lg:text-[82px] 3xl:text-[105px] md:text-4xl text-xl flex justify-center text-center  md:justify-end lg:pb-16 md:pr-11 xl:pr-28 3xl:pr-40 '> 
             <h1>Áreas de atuação</h1>
         </div>
-        <div className='flex flex-col md:flex-row py-5 justify-center items-center md:justify-around 2xl:px-24 3xl:px-36'> 
+        <div className='flex flex-col md:flex-row py-5 justify-center items-center md:justify-around 2xl:px-20 3xl:px-36'> 
     {CARD.map(e =>(
         <Cards role ={e.role} image = {e.image} description = {e.description} />
     ))}
